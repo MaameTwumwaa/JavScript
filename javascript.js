5043841c66a7201b28577c0c1b5e7e37bf6210e0
Adutwumwaa <priseyboat@gmail.com>
2023-05-17 13:31:26 +0000

    first commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+
+
+let numberofhorses=24
+let numberofriders=24
+
+console.log(numberofhorses <= numberofriders)
---
+++ myscript.js
@@ -0,0 +1,47 @@
+//preincreament
+
+let num1= 50
+let num2= 30
+
+console.log(++num1)
+console.log(num1++)
+console.log(num1)
+
+
+//addition
+
+let val1= 50
+let val2= 30
+
+console.log(val1 + val2)
+
+
+//modulus
+ 
+let saf= 70
+let saf2= 3
+
+console.log(saf % saf2);
+
+//comparison operators
+
+let gua1= ('man')
+let gua2= ('woman')
+let gua3= 12
+let gua4= 17
+
+console.log(gua1);
+console.log(gua2);
+console.log(12);
+console.log(17);
+
+
+console.log(gua1 == gua2);
+console.log(gua3 >= gua4);
+console.log( gua1 !== gua2);
+
+//boolean operators
+
+console.log(gua1 && gua2);
+console.log(gua2 || gua1);
+console.log(! false)
